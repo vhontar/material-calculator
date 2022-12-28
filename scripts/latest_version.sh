7c7c209d8229572dbae6f86c7bb0ba702f20a5c2
Vladyslav Hontar <vladislav.hontar@gmail.com>
2022-12-28 21:04:43 +0100

    update bitrise script

--- scripts/latest_version.sh
+++ scripts/latest_version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-PARSED_VERSION=$(cat CHANGELOG.md | sed -r '/^\s*$/d' | awk -F '# ' '{print $2}')
-echo "Extracted version name: $PARSED_VERSION"
-envman add --key LATEST_VERSION_NAME --value "$PARSED_VERSION"
+version=$(cat CHANGELOG.md | sed -r '/^\s*$/d' | awk -F '# ' '{print $2}')
+echo "Extracted version name: $version"
+envman add --key ANDROID_VERSION_NAME --value "$version"
