0b961c0706fc6b291c501ff431426d3606649c13
Vladyslav Hontar <vladislav.hontar@gmail.com>
2022-12-28 13:36:38 +0100

    Material3 migration + create all actions for a calculator

---
+++ app/src/main/java/com/vhontar/materialcalculator/presentation/CalculatorUiAction.kt
@@ -0,0 +1,18 @@
+package com.vhontar.materialcalculator.presentation
+
+import androidx.compose.runtime.Composable
+import com.vhontar.materialcalculator.domain.writer.CalculatorAction
+
+data class CalculatorUiAction(
+    val text: String,
+    val highlightLevel: HighlightLevel,
+    val action: CalculatorAction,
+    val content: @Composable () -> Unit = {  }
+)
+
+sealed interface HighlightLevel {
+    object Neutral: HighlightLevel
+    object SemiHighlighted: HighlightLevel
+    object Highlighted: HighlightLevel
+    object StronglyHighlighted: HighlightLevel
+}
